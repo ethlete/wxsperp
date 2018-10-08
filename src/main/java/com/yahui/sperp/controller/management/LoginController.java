764979be954dd0319bbc3d61a69657e5f9b819ff
Yahui Jiang <ethlete@163.com>
2018-10-08 23:27:37 +0800

    remove spring security and session feature provisionally. generate prototype index.html and dashboard.html

--- src/main/java/com/yahui/sperp/controller/management/LoginController.java
+++ src/main/java/com/yahui/sperp/controller/management/LoginController.java
@@ -1,8 +1,12 @@
 package com.yahui.sperp.controller.management;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,6 +24,13 @@ public class LoginController {
 	public ModelAndView list() {
 		Iterable<Product> products = productRepository.findAll();
 		return new ModelAndView("management/login", "products", products);
+	}
+	
+	@PostMapping("dashboard")
+	public ModelAndView dashboard() {
+		Map<String, String[]> todolist = new HashMap<>();
+		todolist.put("leaveApproval", new String[]{"a", "b", "c"});
+		return new ModelAndView("dashboard", "todolist", todolist);
 	}
 
 }
